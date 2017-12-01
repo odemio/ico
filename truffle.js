cc487cbad0aca0337e8f675e99183156abd50111
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-12-01 14:32:54 +0900

    Remove unecessary comma.

--- truffle.js
+++ truffle.js
@@ -7,22 +7,22 @@ module.exports = {
       live: {
         network_id: 1, // Ethereum public network
         host: "localhost",
-        port: 8545,
+        port: 8545
       },
       testnet: {
         network_id: 3, // Official Ethereum test network (Ropsten)
         host: "localhost",
-        port: 8545,
+        port: 8545
       },
       rinkeby: {
         network_id: 4,
         host: "localhost",
-        port: 8545,
+        port: 8545
       },
       development: {
         host: 'localhost',
         port: 8545,
-        network_id: '*',
+        network_id: '*'
       }
   }
 }
