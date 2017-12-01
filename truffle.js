8325c203a7ba56610bb1b3a91bdff626aa2285ba
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-12-01 14:57:10 +0900

    Remove whitespace.

--- truffle.js
+++ truffle.js
@@ -4,25 +4,25 @@ require('babel-polyfill')
 
 module.exports = {
   networks: {
-      live: {
-        network_id: 1, // Ethereum public network
-        host: "localhost",
-        port: 8545
-      },
-      testnet: {
-        network_id: 3, // Official Ethereum test network (Ropsten)
-        host: "localhost",
-        port: 8545
-      },
-      rinkeby: {
-        network_id: 4,
-        host: "localhost",
-        port: 8545
-      },
-      development: {
-        host: 'localhost',
-        port: 8545,
-        network_id: '*'
-      }
+    live: {
+    network_id: 1, // Ethereum public network
+    host: "localhost",
+    port: 8545
+    },
+    testnet: {
+    network_id: 3, // Official Ethereum test network (Ropsten)
+    host: "localhost",
+    port: 8545
+    },
+    rinkeby: {
+    network_id: 4,
+    host: "localhost",
+    port: 8545
+    },
+    development: {
+    host: 'localhost',
+    port: 8545,
+    network_id: '*'
+    }
   }
 }
