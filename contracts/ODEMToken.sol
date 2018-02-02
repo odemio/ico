7410686d577d798a766618a35830df731859b1f1
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-02-02 22:29:12 +0100

    remove burn function from token contract

--- contracts/ODEMToken.sol
+++ contracts/ODEMToken.sol
@@ -12,17 +12,4 @@ contract ODEMToken is PausableToken, MintableToken {
     string public constant name = "ODEM Token";
     string public constant symbol = "ODEM";
     uint8 public constant decimals = 18;
-
-    event Burn(address indexed burner, uint256 value);
-
-    /**
-     * @dev Burns a specific amount of tokens.
-     * @param _value The amount of token to be burned.
-     */
-    function burn(uint256 _value) public {
-        address burner = msg.sender;
-        balances[burner] = balances[burner].sub(_value);
-        totalSupply = totalSupply.sub(_value);
-        Burn(burner, _value);
-    }
 }
